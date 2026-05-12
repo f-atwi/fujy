7efd6c8d32a3544e31168cd04e0df6c18788f347
Fouad Atwi <fouad.atwi@canonical.com>
2026-05-13 00:38:02 +0200

    move bash completions to hive-core

--- fujy.plugin.bash
+++ fujy.plugin.bash
@@ -1,21 +1,7 @@
 . "${BASH_SOURCE%/*}/fujy"
 
-_fujy_units()
-{
-    juju status 2>/dev/null | awk '/^[a-z][a-z0-9-]*\/[0-9]/{gsub(/\*/, "", $1); print $1}'
-}
+_fujy_units() { juju status 2>/dev/null | awk '/^[a-z][a-z0-9-]*\/[0-9]/{gsub(/\*/, "", $1); print $1}'; }
 
-_fujy_completion()
-{
-    local cur units
-
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-
-    [[ $COMP_CWORD -gt 1 ]] && return
-
-    units="$(_fujy_units | tr '\n' ' ')"
-    COMPREPLY=($(compgen -W "$units" -- "$cur"))
-}
+_fujy_completion() { _fly_complete _fujy_units; }
 
 complete -F _fujy_completion flyjujushell fjujushell flyjujubash fjujubash flyjujuzsh fjujuzsh flyjujuksh fjujuksh
